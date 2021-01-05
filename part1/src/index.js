52984784e65e18e31e00709e437c30d06da249ca
=Martin <=samardzievm@gmail.com>
2021-01-05 22:06:08 +0100

    exercise 1.6

--- part1/src/index.js
+++ part1/src/index.js
@@ -22,16 +22,15 @@ const App = () => {
   const title = "give feedback";
 
   const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
   const handleGood = () => {
     setGood(good + 1)
   }
-
-  const [neutral, setNeutral] = useState(0)
   const handleNeutral = () => {
     setNeutral(neutral + 1)
   }
-  
-  const [bad, setBad] = useState(0)
   const handleBad = () => {
     setBad(bad + 1)
   }
